eaf995162bacd60d60a69b9f91a0307b0fb7fe72
olfatemam <olfat.emam@gmail.com>
2020-07-07 01:27:04 +0300

    Update sidebar.blade.php

--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -4,6 +4,7 @@
 
 <div class="w3-sidebar w3-light-grey w3-bar-block" style="width:10%">
   
+  <a href="{{route('tasks.calendar')}}" class="w3-bar-item w3-button"><i class="fas fa-calendar-check">Calendar</i></a>
   <a href="{{route('tasks.list_filtered', \App\Task::Active)}}" class="w3-bar-item w3-button"><i class="fas fa-tasks">Active</i></a>
   <a href="{{route('tasks.list_filtered', \App\Task::Today)}}" class="w3-bar-item w3-button"><i class="fas fa-calendar-day"> Today</i></a>
   <a href="{{route('tasks.list_filtered', \App\Task::Coming)}}" class="w3-bar-item w3-button"><i class="fas fa-calendar-plus"> Coming</i></a>
